3b7e2fead72eed7c539bf5d4e5f88e4decbbc262
Tanmay Naik <tanmaysnaik@yahoo.in>
2020-01-25 22:43:49 -0500

    Added renderUser()

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -5,28 +5,29 @@
     let $createUserBtn;
     let $deleteUserBtn;
 
-    jQuery(main);
+    let main = () => {
+        rowTemplate = $('.wbdv-template');
+        $createUserBtn = $('.wbdv-create');
 
-    function main() {
-        rowTemplate = jQuery('.wbdv-template');
-        $createUserBtn = jQuery('.wbdv-create');
-        $deleteUserBtn = jQuery('.wbdv-delete');
-        tbody = jQuery('tbody');
+        tbody = $('tbody');
 
         $createUserBtn.click(createUser);
-        $deleteUserBtn.click(function () {
-            console.log('del')
-        });
+
         // $deleteUserBtn.click(deleteUser);
 
-        userService
-            .findAllUsers()
+        findAllUsers()
             .then(renderUsers);
-    }
-    function deleteUser() {
+    };
+
+    let deleteUser = () => {
         console.log('here');
-    }
-    function createUser() {
+    };
+
+    let findAllUsers = () =>
+        userService
+            .findAllUsers();
+
+    let createUser = () => {
         let $usernameFld = $('#usernameFld');
         let $passwordFld = $('#passwordFld');
         let $firstNameFld = $('#firstNameFld');
@@ -48,28 +49,37 @@
 
         userService
             .createUser(user)
-            .then(userService.findAllUsers)
-            .then(renderUsers)
+            .then(renderUser)
             .then(clearFields);
+    };
 
-    }
+    let renderUser = (user) => {
+        const rowClone = rowTemplate.clone();
+        rowClone.removeClass('wbdv-hidden');
+        rowClone.find('.wbdv-username').html(user.username);
+        rowClone.find('.wbdv-first-name').html(user.firstName);
+        rowClone.find('.wbdv-last-name').html(user.lastName);
+        rowClone.find('.wbdv-role').html(user.role);
+        rowClone.prop('id', user._id);
+        tbody.append(rowClone);
+    };
 
-    function renderUsers(users) {
+    let renderUsers = (users) => {
         console.log(users);
         tbody.empty();
         for (let u in users) {
-            const user = users[u];
-            const rowClone = rowTemplate.clone();
-            rowClone.removeClass('wbdv-hidden');
-            rowClone.find('.wbdv-username').html(user.username);
-            rowClone.find('.wbdv-first-name').html(user.firstName);
-            rowClone.find('.wbdv-last-name').html(user.lastName);
-            rowClone.find('.wbdv-role').html(user.role);
-            tbody.append(rowClone);
+            renderUser(users[u]);
         }
-    }
+        $deleteUserBtn = $('.wbdv-delete');
+        $deleteUserBtn.click(function (event) {
+            console.log('del', $(this).closest('tr').attr('id'));
+        });
+        // $deleteUserBtn.click(function (event) {
+        //     console.log('del', $(this).closest('tr').attr('id'));
+        // });
+    };
 
-    function clearFields() {
+    let clearFields = () => {
         let $usernameFld = $('#usernameFld');
         let $passwordFld = $('#passwordFld');
         let $firstNameFld = $('#firstNameFld');
@@ -81,5 +91,6 @@
         $firstNameFld.val('');
         $lastNameFld.val('');
         $roleFld.val('faculty');
-    }
+    };
+    $(main);
 })();
