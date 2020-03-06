59c636011a1388f595299a5ce0d9a51ce79ffecc
Tanmay Naik <tanmaysnaik@yahoo.in>
2020-03-06 15:04:55 -0500

    implemented repository

--- src/main/java/com/example/wbdvsp20tsnaikserverjava/repositories/TopicRepository.java
+++ src/main/java/com/example/wbdvsp20tsnaikserverjava/repositories/TopicRepository.java
@@ -2,14 +2,22 @@ package com.example.wbdvsp20tsnaikserverjava.repositories;
 
 import com.example.wbdvsp20tsnaikserverjava.models.Widget;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicRepository
         extends CrudRepository<Widget, Integer> {
 
-  // SELECT * FROM widgets WHERE topic_id=tid
-//  @Query(value = "SELECT * FROM widgets WHERE topic_id=:tid",
-//          nativeQuery = true)
-//  public List<Widget> findWidgetsForTopic
-//  (@Param("tid") Integer topicId);
+  //   SELECT * FROM widgets WHERE topic_id=tid
+  @Query(value = "SELECT * FROM topics WHERE topics.lesson_id=:lid", nativeQuery = true)
+  List<Widget> findTopicsForLesson(@Param("lid") String lessonId);
+
+  @Query(value = "SELECT * FROM topics", nativeQuery = true)
+  List<Widget> findAllWidgets();
+
+  @Query(value = "SELECT * FROM topics WHERE id=:tid", nativeQuery = true)
+  Widget findWidgetById(@Param("wid") Integer topicId);
 }
--- src/main/java/com/example/wbdvsp20tsnaikserverjava/repositories/WidgetRepository.java
+++ src/main/java/com/example/wbdvsp20tsnaikserverjava/repositories/WidgetRepository.java
@@ -2,14 +2,22 @@ package com.example.wbdvsp20tsnaikserverjava.repositories;
 
 import com.example.wbdvsp20tsnaikserverjava.models.Widget;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WidgetRepository
         extends CrudRepository<Widget, Integer> {
 
-  // SELECT * FROM widgets WHERE topic_id=tid
-//  @Query(value = "SELECT * FROM widgets WHERE topic_id=:tid",
-//          nativeQuery = true)
-//  public List<Widget> findWidgetsForTopic
-//  (@Param("tid") Integer topicId);
+  //   SELECT * FROM widgets WHERE topic_id=tid
+  @Query(value = "SELECT * FROM widgets WHERE topic_id=:tid", nativeQuery = true)
+  List<Widget> findWidgetsForTopic(@Param("tid") Integer topicId);
+
+  @Query(value = "SELECT * FROM widgets", nativeQuery = true)
+  List<Widget> findAllWidgets();
+
+  @Query(value = "SELECT * FROM widgets WHERE id=:wid", nativeQuery = true)
+  Widget findWidgetById(@Param("wid") Integer widgetId);
 }
