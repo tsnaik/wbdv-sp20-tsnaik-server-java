78230e776e975de7714b90534bf45511ad4f160a
Tanmay Naik <tanmaysnaik@yahoo.in>
2020-03-06 14:23:00 -0500

    added topic model

--- src/main/java/com/example/wbdvsp20tsnaikserverjava/models/Topic.java
+++ src/main/java/com/example/wbdvsp20tsnaikserverjava/models/Topic.java
@@ -23,4 +23,44 @@ public class Topic {
   @OneToMany(mappedBy = "topic")
   private List<Widget> widgets;
   private String lessonId;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<Widget> getWidgets() {
+    return widgets;
+  }
+
+  public void setWidgets(List<Widget> widgets) {
+    this.widgets = widgets;
+  }
+
+  public String getLessonId() {
+    return lessonId;
+  }
+
+  public void setLessonId(String lessonId) {
+    this.lessonId = lessonId;
+  }
 }
